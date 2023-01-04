e10526236d4cdb1d1c2856862bb893ee9233edc5
DrinoSan <s.becirbegovic@gmail.com>
2023-01-04 21:27:44 +0100

    theme

--- myapp/src/lib/components/Headers.svelte
+++ myapp/src/lib/components/Headers.svelte
@@ -11,9 +11,9 @@
       <details role="list" dir="rtl">
         <summary aria-haspopup="listbox" role="link" class="secondary">About me</summary>
         <ul role="listbox">
-          <li><a href="/about" data-theme-switcher="auto">About me</a></li>
+          <li><a href="/about" data-theme-switcher="light">About me</a></li>
           <li><a href="/contact" data-theme-switcher="light">Contact</a></li>
-          <li><a href="/form" data-theme-switcher="dark">Send a mail</a></li>
+          <li><a href="/form" data-theme-switcher="light">Send a mail</a></li>
         </ul>
       </details>
     </li>
@@ -30,7 +30,7 @@
       <details role="list" dir="rtl">
         <summary aria-haspopup="listbox" role="link" class="secondary">Login</summary>
         <ul role="listbox">
-          <li><a href="/login" data-theme-switcher="auto">Log-in</a></li>
+          <li><a href="/login" data-theme-switcher="light">Log-in</a></li>
         </ul>
       </details>
     </li>
