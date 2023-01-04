7f65a97d3c0298151824446da339d5936ea7e53d
DrinoSan <s.becirbegovic@gmail.com>
2023-01-04 21:22:40 +0100

    Small adjustment. Should be responsible a bit

--- myapp/src/lib/components/Headers.svelte
+++ myapp/src/lib/components/Headers.svelte
@@ -2,26 +2,44 @@
   import "@picocss/pico";
 </script>
 
-<nav id="navi">
+<nav class="container-fluid" id="navi">
   <ul>
     <li><a href="/">Home</a></li>
   </ul>
   <ul>
-    <li><a href="/about">About me</a></li>
-    <li><a href="/blog">My Blog</a></li>
-    <li><a href="/contact">Contact</a></li>
-    <li><a href="/uses">Markdown</a></li>
-    <li><a href="/login">Log In</a></li>
-    <li><a href="/form">Send a mail</a></li>
-    <li><a href="#" role="button">Button</a></li>
+    <li>
+      <details role="list" dir="rtl">
+        <summary aria-haspopup="listbox" role="link" class="secondary">About me</summary>
+        <ul role="listbox">
+          <li><a href="/about" data-theme-switcher="auto">About me</a></li>
+          <li><a href="/contact" data-theme-switcher="light">Contact</a></li>
+          <li><a href="/form" data-theme-switcher="dark">Send a mail</a></li>
+        </ul>
+      </details>
+    </li>
+    <li>
+      <details role="list" dir="rtl">
+        <summary aria-haspopup="listbox" role="link" class="secondary">Blog</summary>
+        <ul role="listbox">
+          <li><a href="/blog">My Blog</a></li>
+          <li><a href="/uses">Markdown</a></li>
+        </ul>
+      </details>
+    </li>
+    <li>
+      <details role="list" dir="rtl">
+        <summary aria-haspopup="listbox" role="link" class="secondary">Login</summary>
+        <ul role="listbox">
+          <li><a href="/login" data-theme-switcher="auto">Log-in</a></li>
+        </ul>
+      </details>
+    </li>
   </ul>
 </nav>
 
-<style>
+<style> 
   #navi {
-    padding: 1rem;
-    display: flex;
     background: #333333;
-    justify-content: space-between;
   }
+
 </style>
--- myapp/src/routes/contact/+page.svelte
+++ myapp/src/routes/contact/+page.svelte
@@ -1,1 +1,9 @@
 <h1>Contact me</h1>
+
+<details open>
+  <summary>Details</summary>
+  <ul>
+    <li>mail: sbed@protonmail.com</li>
+    <li>twitter: <a href="https://twitter.com/DrinoSan_">Tweet</a></li>
+  </ul>
+</details>
