9eb7c19246ebe037fd9efbed47598e2a6ee96133
DrinoSan <s.becirbegovic@gmail.com>
2023-01-05 01:32:05 +0100

    Fix goback

--- myapp/src/routes/blog/[slug]/+page.svelte
+++ myapp/src/routes/blog/[slug]/+page.svelte
@@ -3,10 +3,17 @@
   export let data: PageData;
 </script>
 
-  <input type="button" class="contrast outline" value="Go back!" onclick="history.back()">
+  <a href="/blog" id="back" role="button" class="contrast outline">Go back!</a>
   
 <article>
   <h1>{data.title}</h1>
   <p>Published: {data.date}</p>
   <svelte:component this={data.content} />
 </article>
+
+
+<style>
+  #back{
+    width: 100%;
+  }
+</style>
